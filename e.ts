32c1f06176364b3d9bdb9f39cde1a84b32729b61
Innei <i@innei.in>
2024-08-14 17:15:45 +0800

    Merge
    chore: Release v3.3.3
    Signed-off-by: Innei <i@innei.in>

--- e.ts
+++ e.ts
@@ -1,10 +1,4 @@
-const commitMessage = 'chore: Release v1.11.1'
-const semverRegex =
-  /chore: Release (v[0-9]+\.[0-9]+\.[0-9]+(-[0-9A-Za-z-.]+)?)/;
-const match = commitMessage.match(semverRegex);
-if (match) {
-  console.log(`::set-output name=version::${match[1]}`);
-} else {
-  console.log("No SemVer found in commit body.");
-}
  
+import fs from 'fs'
+const version = 'test'
+fs.appendFileSync(process.env.GITHUB_ENV, `version=${version}\n`);
