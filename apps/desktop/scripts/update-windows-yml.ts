d8047908c20d1eb3d737b0343fded9d5bf0bdb66
DIYgod <i@diygod.me>
2025-03-25 23:04:28 +0800

    update

--- apps/desktop/scripts/update-windows-yml.ts
+++ apps/desktop/scripts/update-windows-yml.ts
@@ -1,10 +1,12 @@
 import crypto from "node:crypto"
 import fs from "node:fs"
+import { fileURLToPath, resolve } from "node:url"
 
 import yaml from "js-yaml"
 
-const basePath = "apps/desktop/out/make/squirrel.windows/x64"
-const ymlPath = `${basePath}/latest.yml`
+const __dirname = fileURLToPath(new URL(".", import.meta.url))
+const basePath = resolve(__dirname, "../out/make/squirrel.windows/x64")
+const ymlPath = resolve(basePath, "latest.yml")
 
 const yml = yaml.load(fs.readFileSync(ymlPath, "utf8")) as {
   version?: string
@@ -18,9 +20,9 @@ const yml = yaml.load(fs.readFileSync(ymlPath, "utf8")) as {
 
 const file = yml.files[0].url
 
-const fileData = fs.readFileSync(`${basePath}/${file}`)
+const fileData = fs.readFileSync(resolve(basePath, file))
 const hash = crypto.createHash("sha512").update(fileData).digest("base64")
-const { size } = fs.statSync(`${basePath}/${file}`)
+const { size } = fs.statSync(resolve(basePath, file))
 
 yml.files[0].sha512 = hash
 yml.files[0].size = size
