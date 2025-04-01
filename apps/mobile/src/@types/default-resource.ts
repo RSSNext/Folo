e0a1da5b26a19f790a52f2952bd818011b81654b
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-04-01 22:43:28 +0800

    translation for mobile content

--- apps/mobile/src/@types/default-resource.ts
+++ apps/mobile/src/@types/default-resource.ts
@@ -48,8 +48,26 @@ import lang_tr from "@locales/lang/tr.json"
 import lang_zhCN from "@locales/lang/zh-CN.json"
 import lang_zhHK from "@locales/lang/zh-HK.json"
 import lang_zhTW from "@locales/lang/zh-TW.json"
+import arDZ from "@locales/mobile/default/ar-DZ.json"
+import arIQ from "@locales/mobile/default/ar-IQ.json"
+import arKW from "@locales/mobile/default/ar-KW.json"
+import arMA from "@locales/mobile/default/ar-MA.json"
+import arSA from "@locales/mobile/default/ar-SA.json"
+import arTN from "@locales/mobile/default/ar-TN.json"
+import de from "@locales/mobile/default/de.json"
 import en from "@locales/mobile/default/en.json"
+import es from "@locales/mobile/default/es.json"
+import fi from "@locales/mobile/default/fi.json"
+import fr from "@locales/mobile/default/fr.json"
+import it from "@locales/mobile/default/it.json"
+import ja from "@locales/mobile/default/ja.json"
+import ko from "@locales/mobile/default/ko.json"
+import pt from "@locales/mobile/default/pt.json"
+import ru from "@locales/mobile/default/ru.json"
+import tr from "@locales/mobile/default/tr.json"
 import zhCN from "@locales/mobile/default/zh-CN.json"
+import zhHK from "@locales/mobile/default/zh-HK.json"
+import zhTW from "@locales/mobile/default/zh-TW.json"
 
 import type { MobileSupportedLanguages, ns } from "./constants"
 
@@ -58,31 +76,37 @@ export const defaultResources = {
   // @keep-sorted
   "ar-DZ": {
     common: common_ardz,
+    default: arDZ,
     lang: lang_ardz,
   },
   // @keep-sorted
   "ar-IQ": {
     common: common_ariq,
+    default: arIQ,
     lang: lang_ariq,
   },
   // @keep-sorted
   "ar-KW": {
     common: common_arkw,
+    default: arKW,
     lang: lang_arkw,
   },
   // @keep-sorted
   "ar-MA": {
     common: common_arma,
+    default: arMA,
     lang: lang_arma,
   },
   // @keep-sorted
   "ar-SA": {
     common: common_arsa,
+    default: arSA,
     lang: lang_arsa,
   },
   // @keep-sorted
   "ar-TN": {
     common: common_artn,
+    default: arTN,
     lang: lang_artn,
   },
   // @keep-sorted
@@ -95,18 +119,21 @@ export const defaultResources = {
   // @keep-sorted
   "zh-HK": {
     common: common_zhHK,
+    default: zhHK,
     errors: errors_zhHK,
     lang: lang_zhHK,
   },
   // @keep-sorted
   "zh-TW": {
     common: common_zhTW,
+    default: zhTW,
     errors: errors_zhTW,
     lang: lang_zhTW,
   },
   // @keep-sorted
   de: {
     common: common_de,
+    default: de,
     errors: errors_de,
     lang: lang_de,
   },
@@ -120,50 +147,59 @@ export const defaultResources = {
   // @keep-sorted
   es: {
     common: common_es,
+    default: es,
     lang: lang_es,
   },
   // @keep-sorted
   fi: {
     common: common_fi,
+    default: fi,
     lang: lang_fi,
   },
   // @keep-sorted
   fr: {
     common: common_fr,
+    default: fr,
     errors: errors_fr,
     lang: lang_fr,
   },
   // @keep-sorted
   it: {
     common: common_it,
+    default: it,
     lang: lang_it,
   },
   // @keep-sorted
   ja: {
     common: common_ja,
+    default: ja,
     errors: errors_ja,
     lang: lang_ja,
   },
   // @keep-sorted
   ko: {
     common: common_ko,
+    default: ko,
     errors: errors_ko,
     lang: lang_ko,
   },
   // @keep-sorted
   pt: {
     common: common_pt,
+    default: pt,
     lang: lang_pt,
   },
   // @keep-sorted
   ru: {
     common: common_ru,
+    default: ru,
     errors: errors_ru,
     lang: lang_ru,
   },
   // @keep-sorted
   tr: {
     common: common_tr,
+    default: tr,
     errors: errors_tr,
     lang: lang_tr,
   },
