e1ef1f4d44c53bfdb2ca2b3ed5f327ca9801509f
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-04-01 18:41:07 +0800

    Revert "better default"
    This reverts commit 272ca1501e80cc982e39abc1c4279657eb3a0b6b.

--- apps/ssr/client/atoms/settings/general.ts
+++ apps/ssr/client/atoms/settings/general.ts
@@ -1,29 +1,11 @@
-import { currentSupportedLanguages } from "@client/@types/constants"
 import type { GeneralSettings } from "@follow/shared/settings/interface"
-import { getStorageNS } from "@follow/utils/ns"
-import LanguageDetector from "i18next-browser-languagedetector"
 
 import { createSettingAtom } from "./helper"
-
-const I18N_LOCALE_KEY = getStorageNS("I18N_LOCALE")
-
-const getDefaultLanguage = (): string => {
-  const languageDetector = new LanguageDetector(null, {
-    order: ["querystring", "localStorage", "navigator"],
-    lookupQuerystring: "lng",
-    lookupLocalStorage: I18N_LOCALE_KEY,
-    caches: ["localStorage"],
-  })
-  const userLang = languageDetector.detect()
-  if (!userLang) return "en"
-  const firstUserLang = Array.isArray(userLang) ? userLang[0]! : userLang
-  return currentSupportedLanguages.includes(firstUserLang) ? firstUserLang : "en"
-}
 
 const createDefaultSettings = (): Partial<GeneralSettings> => ({
   // App
   appLaunchOnStartup: false,
-  language: getDefaultLanguage(),
+  language: "en",
   // Data control
   dataPersist: true,
   sendAnonymousData: true,
