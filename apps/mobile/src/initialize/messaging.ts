94e698aa608d19b507ab2e52e8f7945abcb96125
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-04-07 19:00:10 +0800

    cache messaging token

--- apps/mobile/src/initialize/messaging.ts
+++ apps/mobile/src/initialize/messaging.ts
@@ -1,8 +1,26 @@
 import messaging from "@react-native-firebase/messaging"
 
 import { apiClient } from "../lib/api-fetch"
+import { kv } from "../lib/kv"
 import { requestNotificationPermission } from "../lib/permission"
 import { whoami } from "../store/user/getters"
+
+const FIREBASE_MESSAGING_TOKEN_STORAGE_KEY = "firebase_messaging_token"
+
+async function saveMessagingToken() {
+  const token = await messaging().getToken()
+  const storedToken = await kv.get(FIREBASE_MESSAGING_TOKEN_STORAGE_KEY)
+  if (storedToken === token) {
+    return
+  }
+  await apiClient.messaging.$post({
+    json: {
+      token,
+      channel: "mobile",
+    },
+  })
+  kv.set(FIREBASE_MESSAGING_TOKEN_STORAGE_KEY, token)
+}
 
 export const initMessaging = () => {
   const user = whoami()
@@ -10,15 +28,6 @@ export const initMessaging = () => {
     return
   }
 
-  messaging()
-    .getToken()
-    .then((token) => {
-      apiClient.messaging.$post({
-        json: {
-          token,
-          channel: "mobile",
-        },
-      })
-    })
+  saveMessagingToken()
   requestNotificationPermission()
 }
