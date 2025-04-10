eee68e2a76158b0a9964cb0d4c737bdd0724f580
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-04-10 15:50:02 +0800

    unread count for view

--- apps/mobile/src/modules/subscription/items/UnreadCount.tsx
+++ apps/mobile/src/modules/subscription/items/UnreadCount.tsx
@@ -1,15 +1,34 @@
 import { cn } from "@follow/utils"
-import { Text, View } from "react-native"
+import type { AnimatedProps } from "react-native-reanimated"
+import Animated from "react-native-reanimated"
 
 import { useUISettingKey } from "@/src/atoms/settings/ui"
 
-export function UnreadCount({ unread, className }: { unread?: number; className?: string }) {
+export function UnreadCount({
+  unread,
+  className,
+  textClassName,
+  dotClassName,
+  ...rest
+}: { unread?: number } & AnimatedProps<{
+  className?: string
+  textClassName?: string
+  dotClassName?: string
+}>) {
   const showUnreadCount = useUISettingKey("showUnreadCountViewAndSubscriptionMobile")
 
   if (!unread) return null
   return showUnreadCount ? (
-    <Text className={cn("text-tertiary-label text-xs", className)}>{unread}</Text>
+    <Animated.Text
+      className={cn("text-tertiary-label text-xs", className, textClassName)}
+      {...rest}
+    >
+      {unread}
+    </Animated.Text>
   ) : (
-    <View className={cn("bg-tertiary-label size-1 rounded-full", className)} />
+    <Animated.View
+      className={cn("bg-tertiary-label size-1 rounded-full", className, dotClassName)}
+      {...rest}
+    />
   )
 }
