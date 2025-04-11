86d0925af6606b4396b5c2a4c68932e2b12a7121
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-04-11 17:22:17 +0800

    correct type

--- apps/desktop/src/renderer/src/modules/entry-content/components/ImageGalleryContent.tsx
+++ apps/desktop/src/renderer/src/modules/entry-content/components/ImageGalleryContent.tsx
@@ -1,5 +1,3 @@
-import type { MediaModel } from "@follow/models"
-
 import { useEntry } from "~/store/entry"
 
 import { ImageGallery } from "../actions/picture-gallery"
@@ -8,5 +6,5 @@ export const ImageGalleryContent = ({ entryId }: { entryId: string }) => {
   const images = useEntry(entryId, (entry) => entry.entries.media)
   // images?.length && images.length > 5
   // We don't need to check here, we already check in the action
-  return <ImageGallery images={images as any as MediaModel[]} />
+  return <ImageGallery images={images} />
 }
