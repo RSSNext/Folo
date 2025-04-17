8107fcd6ed1c800941b16d62b4de1c86ac53cd0b
Vixb <xzx8023@outlook.com>
2025-04-17 16:19:00 +0800

    feat: disable core updater for process from custom location

--- apps/desktop/src/main/src/updater/configs.ts
+++ apps/desktop/src/main/src/updater/configs.ts
@@ -1,9 +1,13 @@
 import { DEV } from "@follow/shared/constants"
+import path from "path"
 
 export const appUpdaterConfig = {
   // Disable renderer hot update will trigger app update when available
   enableRenderHotUpdate: !DEV,
-  enableCoreUpdate: !process.mas && !process.windowsStore,
+  enableCoreUpdate: (!process.mas && !process.windowsStore
+    // Disable core update if platfrom is windows and application is't executing from default installion path.
+    && !(process.platform == "win32" && path.dirname(path.dirname(process.execPath)) != process.env.LOCALAPPDATA)
+  ),
   // Disable app update will also disable renderer hot update and core update
   enableAppUpdate: !DEV,
 
