cf60b9c8f6bf104bd7b1a3760904769e37a0f758
Vixb <xzx8023@outlook.com>
2025-04-17 17:27:26 +0800

    chore: update Updater check logic

--- apps/desktop/src/main/src/updater/configs.ts
+++ apps/desktop/src/main/src/updater/configs.ts
@@ -1,13 +1,15 @@
 import { DEV } from "@follow/shared/constants"
+import { isWindows } from "../env"
 import path from "path"
 
 export const appUpdaterConfig = {
   // Disable renderer hot update will trigger app update when available
   enableRenderHotUpdate: !DEV,
-  enableCoreUpdate: (!process.mas && !process.windowsStore
+  enableCoreUpdate:
+    !process.mas &&
+    !process.windowsStore &&
     // Disable core update if platfrom is windows and application is't executing from default installion path.
-    && !(process.platform == "win32" && path.dirname(path.dirname(process.execPath)) != process.env.LOCALAPPDATA)
-  ),
+    !(isWindows && path.resolve(process.execPath, "../../") != process.env.LOCALAPPDATA),
   // Disable app update will also disable renderer hot update and core update
   enableAppUpdate: !DEV,
 
