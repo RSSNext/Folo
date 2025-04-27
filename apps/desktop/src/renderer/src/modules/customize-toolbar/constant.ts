251fae97d3af17fbf67bfaa8f7e2aa2004592ebe
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-04-27 11:09:43 +0800

    hide in custom order

--- apps/desktop/src/renderer/src/modules/customize-toolbar/constant.ts
+++ apps/desktop/src/renderer/src/modules/customize-toolbar/constant.ts
@@ -20,13 +20,6 @@ export const entryItemHideInHeader = new Set<string>([
   COMMAND_ID.entry.readBelow,
 ])
 
-export const entryItemHideInContextMenu = new Set<string>([
-  COMMAND_ID.entry.viewSourceContent,
-  COMMAND_ID.entry.toggleAISummary,
-  COMMAND_ID.entry.toggleAITranslation,
-  COMMAND_ID.settings.customizeToolbar,
-])
-
 export const DEFAULT_ACTION_ORDER: ToolbarActionOrder = {
   main: Object.values(COMMAND_ID.entry).filter((id) => !entryItemInMore.has(id)),
   more: [
