bb90ec1619897ad6ff638a4c6f73c6d150c998f9
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-04-27 17:06:00 +0800

    update

--- apps/desktop/src/renderer/src/modules/entry-column/hooks/useMarkAll.ts
+++ apps/desktop/src/renderer/src/modules/entry-column/hooks/useMarkAll.ts
@@ -1,55 +1,12 @@
-import { useCallback } from "react"
-
-import { getRouteParams, useRouteParams } from "~/hooks/biz/useRouteParams"
+import { getRouteParams } from "~/hooks/biz/useRouteParams"
 import { getFolderFeedsByFeedId, subscriptionActions } from "~/store/subscription"
-import { useFolderFeedsByFeedId } from "~/store/subscription/hooks"
 
 export interface MarkAllFilter {
   startTime: number
   endTime: number
 }
-export const useMarkAllByRoute = (filter?: MarkAllFilter) => {
-  const routerParams = useRouteParams()
-  const { feedId, view, inboxId, listId } = routerParams
-  const folderIds = useFolderFeedsByFeedId({
-    feedId,
-    view,
-  })
 
-  return useCallback(async () => {
-    if (!routerParams) return
-
-    if (typeof routerParams.feedId === "number" || routerParams.isAllFeeds) {
-      subscriptionActions.markReadByView(view, filter)
-    } else if (inboxId) {
-      subscriptionActions.markReadByFeedIds({
-        inboxId,
-        view,
-        filter,
-      })
-    } else if (listId) {
-      subscriptionActions.markReadByFeedIds({
-        listId,
-        view,
-        filter,
-      })
-    } else if (folderIds?.length) {
-      subscriptionActions.markReadByFeedIds({
-        feedIds: folderIds,
-        view,
-        filter,
-      })
-    } else if (routerParams.feedId) {
-      subscriptionActions.markReadByFeedIds({
-        feedIds: routerParams.feedId?.split(","),
-        view,
-        filter,
-      })
-    }
-  }, [routerParams, inboxId, listId, folderIds, view, filter])
-}
-
-export const markAllByRoute = (filter?: MarkAllFilter) => {
+export const markAllByRoute = async (filter?: MarkAllFilter) => {
   const routerParams = getRouteParams()
   const { feedId, view, inboxId, listId } = routerParams
   const folderIds = getFolderFeedsByFeedId({
