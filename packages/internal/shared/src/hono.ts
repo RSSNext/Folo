15f6faa1266973b73dbc9a3bf0993826cac3bfe3
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-05-21 11:35:24 +0800

    update

--- packages/internal/shared/src/hono.ts
+++ packages/internal/shared/src/hono.ts
@@ -14887,7 +14887,7 @@ declare const _routes: hono_hono_base.HonoBase<Env, ({
                     isCollection?: boolean | undefined;
                     isArchived?: boolean | undefined;
                     withContent?: boolean | undefined;
-                    hidePrivate?: boolean | undefined;
+                    excludePrivate?: boolean | undefined;
                 };
             };
             output: {
@@ -15557,7 +15557,7 @@ declare const _routes: hono_hono_base.HonoBase<Env, ({
                     listId?: string | undefined;
                     feedIdList?: string[] | undefined;
                     inboxId?: string | undefined;
-                    hidePrivate?: boolean | undefined;
+                    excludePrivate?: boolean | undefined;
                     startTime?: number | undefined;
                     endTime?: number | undefined;
                 };
