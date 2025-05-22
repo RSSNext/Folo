6a10ad6d8be8a6e40745885e440f743cbe0db10c
Innei <tukon479@gmail.com>
2025-05-22 22:44:09 +0800

    update
    Signed-off-by: Innei <tukon479@gmail.com>

--- apps/desktop/layer/renderer/src/modules/command/shortcuts/SettingShortcuts.tsx
+++ apps/desktop/layer/renderer/src/modules/command/shortcuts/SettingShortcuts.tsx
@@ -28,9 +28,52 @@ export const ShortcutsGuideline = () => {
   )
 }
 
+export const ShortcutSetting = () => {
+  const { t } = useTranslation("shortcuts")
+  const commandShortcuts = useCommandShortcutItems()
+
+  return (
+    <div>
+      <p className="mb-6 mt-4 space-y-2 text-sm">{t("settings.shortcuts.description")}</p>
+      {Object.entries(commandShortcuts).map(([type, commands]) => (
+        <section key={type} className="mb-8">
+          <div className="text-text border-border mb-4 border-b pb-2 text-base font-medium">
+            {t(type as CommandCategory)}
+          </div>
+          <div className="space-y-4">
+            {commands.map((commandId) => (
+              <EditableCommandShortcutItem key={commandId} commandId={commandId} />
+            ))}
+          </div>
+        </section>
+      ))}
+    </div>
+  )
+}
+
+const EditableCommandShortcutItem = memo(({ commandId }: { commandId: FollowCommandId }) => {
+  const command = useCommand(commandId)
+  const commandShortcuts = useCommandShortcuts()
+  // const [isEditing, setIsEditing] = useState(false)
+  if (!command) return null
+  return (
+    <div className={"flex h-8 items-center justify-between py-1.5"}>
+      <div className="text-text text-sm">{command.label.title}</div>
+      {!!command.label.description && (
+        <small className="text-text-secondary text-xs">{command.label.description}</small>
+      )}
+      <div>
+        <KbdCombined kbdProps={{ wrapButton: false }} joint={false}>
+          {commandShortcuts[commandId]}
+        </KbdCombined>
+      </div>
+    </div>
+  )
+})
 const CommandShortcutItem = memo(({ commandId }: { commandId: FollowCommandId }) => {
   const command = useCommand(commandId)
   const commandShortcuts = useCommandShortcuts()
+
   if (!command) return null
   return (
     <div className={"odd:bg-fill-quinary flex h-9 items-center justify-between px-3 py-1.5"}>
