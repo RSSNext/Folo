d597d72d22d5b33d86edbe05e9342c7ec36bd1f4
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-05-23 11:52:07 +0800

    Revert "try fix test"
    This reverts commit 6f56f827f2f8446e17ad74befe56483efff8f421.

--- apps/desktop/layer/renderer/src/store/subscription/index.ts
+++ apps/desktop/layer/renderer/src/store/subscription/index.ts
@@ -1,5 +1,4 @@
-// eslint-disable-next-line simple-import-sort/exports
-export * from "./store"
 export * from "./getters"
 export * from "./hooks"
 export * from "./selector"
+export * from "./store"
