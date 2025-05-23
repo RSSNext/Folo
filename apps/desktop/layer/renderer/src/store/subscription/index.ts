6f56f827f2f8446e17ad74befe56483efff8f421
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-05-23 11:47:33 +0800

    try fix test

--- apps/desktop/layer/renderer/src/store/subscription/index.ts
+++ apps/desktop/layer/renderer/src/store/subscription/index.ts
@@ -1,4 +1,5 @@
+// eslint-disable-next-line simple-import-sort/exports
+export * from "./store"
 export * from "./getters"
 export * from "./hooks"
 export * from "./selector"
-export * from "./store"
