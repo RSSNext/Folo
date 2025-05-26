fa99d233e9c54cd5e6049dcf37458b78c45c61d5
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-05-26 11:57:51 +0800

    type check

--- packages/internal/database/src/db.ts
+++ packages/internal/database/src/db.ts
@@ -1,9 +1,6 @@
-import type { SQLiteDatabase } from "expo-sqlite"
-import type { SQLocalDrizzle } from "sqlocal/drizzle"
-
 import type { DB } from "./types"
 
-export declare const sqlite: SQLiteDatabase | SQLocalDrizzle
+export declare const sqlite: unknown
 export declare const db: DB
 export declare function initializeDb(): void
 export declare function migrateDb(): Promise<void>
