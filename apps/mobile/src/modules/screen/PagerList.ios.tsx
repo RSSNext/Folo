e735ba25551770beafd97205e1143f8cec9c231c
lawvs <18554747+lawvs@users.noreply.github.com>
2025-06-19 15:13:12 +0800

    fix: enhance PagerList to synchronize drag progress with timeline selection

--- apps/mobile/src/modules/screen/PagerList.ios.tsx
+++ apps/mobile/src/modules/screen/PagerList.ios.tsx
@@ -3,8 +3,9 @@ import { useTypeScriptHappyCallback } from "@follow/hooks"
 import { useViewWithSubscription } from "@follow/store/subscription/hooks"
 import { EventBus } from "@follow/utils/event-bus"
 import * as Haptics from "expo-haptics"
-import { useCallback, useEffect, useId, useMemo, useRef, useState } from "react"
+import { useCallback, useId, useLayoutEffect, useMemo, useRef, useState } from "react"
 import type { StyleProp, ViewStyle } from "react-native"
+import { clamp, withTiming } from "react-native-reanimated"
 
 import { PagerView } from "@/src/components/native/PagerView"
 import type { PagerRef } from "@/src/components/native/PagerView/specs"
@@ -32,13 +33,15 @@ export function PagerList({
   const rid = useId()
   const dragProgress = useTimelineSelectorDragProgress()
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     return EventBus.subscribe("SELECT_TIMELINE", (data) => {
       if (data.target !== rid) {
-        pagerRef.current?.setPage(activeViews.indexOf(data.view.viewId))
+        const targetIndex = activeViews.indexOf(data.view.viewId)
+        pagerRef.current?.setPage(targetIndex)
+        dragProgress.set(withTiming(targetIndex))
       }
     })
-  }, [activeViews, pagerRef, rid])
+  }, [activeViews, dragProgress, pagerRef, rid])
   const [dragging, setDragging] = useState(false)
 
   return (
@@ -46,12 +49,21 @@ export function PagerList({
       ref={pagerRef}
       initialPageIndex={initialPageIndex}
       onScroll={(percent, direction, position) => {
-        const progress =
-          percent * (direction === "left" ? -1 : direction === "right" ? 1 : 0) + position
+        const progress = clamp(
+          percent * (direction === "left" ? -1 : direction === "right" ? 1 : 0) + position,
+          0,
+          activeViews.length - 1,
+        )
         dragProgress.set(progress)
       }}
       onScrollBegin={useCallback(() => setDragging(true), [])}
-      onScrollEnd={useCallback(() => setDragging(false), [])}
+      onScrollEnd={useCallback(
+        (index: number) => {
+          setDragging(false)
+          dragProgress.set(withTiming(index))
+        },
+        [dragProgress],
+      )}
       pageContainerClassName="flex-1"
       containerClassName="flex-1 absolute inset-0"
       containerStyle={style}
