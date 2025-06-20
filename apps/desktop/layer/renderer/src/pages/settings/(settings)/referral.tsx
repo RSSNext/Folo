502a0dc8152bef1e77cc08be95d77fdefc20aba7
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-06-20 16:42:58 +0800

    update

--- apps/desktop/layer/renderer/src/pages/settings/(settings)/referral.tsx
+++ apps/desktop/layer/renderer/src/pages/settings/(settings)/referral.tsx
@@ -3,7 +3,7 @@ import { SettingsTitle } from "~/modules/settings/title"
 import { defineSettingPageData } from "~/modules/settings/utils"
 
 const iconName = "i-mgc-love-cute-re"
-const priority = 1070
+const priority = (1000 << 3) + 20
 
 export const loader = defineSettingPageData({
   icon: iconName,
