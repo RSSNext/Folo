21b38d7a00ac7f4929c138fec75e978c67d4acb8
Konv Suu <2583695112@qq.com>
2025-07-02 10:33:31 +0800

    feat(desktop): enhance link handling

--- apps/desktop/layer/renderer/src/modules/entry-column/layouts/EntryItemWrapper.tsx
+++ apps/desktop/layer/renderer/src/modules/entry-column/layouts/EntryItemWrapper.tsx
@@ -86,6 +86,15 @@ export const EntryItemWrapper: FC<
   }, [entry?.id])
   const handleClick = useCallback(
     (e) => {
+      const target = e.target as HTMLElement
+      const targetTag = target.tagName
+
+      if (["A"].includes(targetTag)) {
+        const href = target.getAttribute("href")
+        href && window.open(href, "_blank")
+        return
+      }
+
       e.preventDefault()
       e.stopPropagation()
 
